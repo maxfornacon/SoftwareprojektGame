92f66640c248b053299a6980d264eac5bd985d12
Maximilian Fornaçon <maximilian.fornacon@gmail.com>
2018-11-13 23:10:14 +0100

    final

--- game.py
+++ game.py
@@ -7,12 +7,13 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self):
         super().__init__()
-
+        self.setWindowTitle('Game')
         self.game_widget = Game(parent=self)
         self.setCentralWidget(self.game_widget)
 
         menubar = self.menuBar()
         settingsMenu = menubar.addMenu('&Settings')
+
         opt1 = QtWidgets.QAction('3x3', self)
         opt2 = QtWidgets.QAction('4x4', self)
         opt3 = QtWidgets.QAction('reset', self)
@@ -25,14 +26,13 @@ class MainWindow(QtWidgets.QMainWindow):
         opt2.triggered.connect(lambda: self.game_widget.setupGUI(4))
         opt3.triggered.connect(self.game_widget.reset)
 
-        self.setWindowTitle('Game')
-
 
 class Game(QtWidgets.QWidget):
+    n = 3
+
     def __init__(self, parent=None):
         super().__init__(parent)
-
-        self.setLayout(self.setupGUI(4))
+        self.setupGUI(Game.n)
 
     def buttonClicked(self):
         button = self.sender()
@@ -42,13 +42,13 @@ class Game(QtWidgets.QWidget):
         if location[0] - 1 >= 0:
             buttonAbove = self.layout().itemAtPosition(location[0] - 1, location[1]).widget()
             self.changeColor(buttonAbove)
-        if location[0] + 1 <= 2:
+        if location[0] + 1 <= Game.n - 1:
             buttonBelow = self.layout().itemAtPosition(location[0] + 1, location[1]).widget()
             self.changeColor(buttonBelow)
         if location[1] - 1 >= 0:
             buttonLeft = self.layout().itemAtPosition(location[0], location[1] - 1).widget()
             self.changeColor(buttonLeft)
-        if location[1] + 1 <= 2:
+        if location[1] + 1 <= Game.n - 1:
             buttonRight = self.layout().itemAtPosition(location[0], location[1] + 1).widget()
             self.changeColor(buttonRight)
         self.checkWin()
@@ -56,37 +56,56 @@ class Game(QtWidgets.QWidget):
     def changeColor(self, button):
         color = button.palette().button().color().name()
         if color == "#ffffff":
-            button.setStyleSheet("background-color: #d3d3d3; border: 2px solid #d3d3d3;")
-        elif color == "#d3d3d3":
-            button.setStyleSheet("background-color: white; border: 2px solid #d3d3d3;")
+            button.setStyleSheet("background-color: #d4d4d4; border: 2px solid #d4d4d4;")
+        elif color == "#d4d4d4":
+            button.setStyleSheet("background-color: white; border: 2px solid #d4d4d4;")
 
     def checkWin(self):
         won = True
-        for i in range(9):
+        for i in range(Game.n * Game.n):
             button = self.layout().itemAt(i).widget()
             color = button.palette().button().color().name()
             if color == "#ffffff":
                 won = None
                 break
         if won:
-            QMessageBox.about(self, "Gewonnen", "Du hast gewonnen!")
+            msgBox = QMessageBox()
+            msgBox.about(self, "Gewonnen", "Du hast gewonnen!")
             self.reset()
 
     def reset(self):
-        for i in range(12):
+        for i in range(Game.n * Game.n):
             button = self.layout().itemAt(i).widget()
-            button.setStyleSheet("background-color: #ffffff; border: 2px solid #d3d3d3")
+            button.setStyleSheet("background-color: #ffffff; border: 2px solid #d4d4d4")
 
     def setupGUI(self, n):
-        grid = QtWidgets.QGridLayout()
+        if n == 3:
+            self.setFixedSize(330, 330)
+            self.parent().adjustSize()
+        else:
+            self.setFixedSize(436, 436)
+        Game.n = n
+        self.clearLayout(self.layout())
+        self.setLayout(QtWidgets.QGridLayout())
+
         for i in range(n):
             for j in range(n):
                 button = QtWidgets.QPushButton(" ")
                 button.setFixedSize(100, 100)
-                button.setStyleSheet("background-color: white; border: 2px solid #d3d3d3;")
+                button.setStyleSheet("background-color: white; border: 2px solid #d4d4d4;")
                 button.clicked.connect(self.buttonClicked)
-                grid.addWidget(button, i, j)
-        return grid
+                self.layout().addWidget(button, i, j)
+
+    def clearLayout(self, layout):
+        if layout is not None:
+            while layout.count():
+                item = layout.takeAt(0)
+                widget = item.widget()
+                if widget is not None:
+                    widget.deleteLater()
+                else:
+                    self.clearLayout(item.layout())
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
